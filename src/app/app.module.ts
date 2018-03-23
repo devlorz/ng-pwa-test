fa390b9e56876acba2517babdbdccbacd700a1aa
Nutti Saelor <leelorz6@gmail.com>
2018-03-23 16:02:39 +0700

    add ng-material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from "./app-routing.module";
 
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from "@angular/service-worker";
+import { AppComponent } from "./app.component";
 
-import { environment } from '../environments/environment';
+import { environment } from "../environments/environment";
+import { NgMaterialModule } from "./angular-material.module";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
+    NgMaterialModule,
+    ServiceWorkerModule.register("/ngsw-worker.js", {
+      enabled: environment.production
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
